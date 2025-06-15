5baa7831b4da69b87a1c255ea498a379d9086c92
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-15 21:22:46 +0500

    Add sign-up action with validation and error handling

---
+++ src/app/actions/auth/sign-up-action.ts
@@ -0,0 +1,46 @@
+"use server"
+
+import { auth } from "@/lib/auth";
+import { signupSchema } from "@/lib/validations";
+import { APIError } from "better-auth/api";
+import { z } from "zod";
+
+
+export const signupAction = async (formData: z.infer<typeof signupSchema>) => {
+  const validatedData = signupSchema.safeParse(formData)
+
+  if (!validatedData.success) {
+    return {
+      success: false,
+      error: "Invalid data passed"
+    }
+  }
+
+  const { name, email, password } = validatedData.data
+
+  try {
+    await auth.api.signUpEmail({
+      body: {
+        name: name,
+        email: email,
+        password: password,
+      }
+    })
+
+    return {
+      success: true,
+      message: "Signed up successfully"
+    }
+  } catch (error) {
+    if (error instanceof APIError) {
+      return {
+        success: false,
+        error: error.message
+      }
+    }
+    return {
+      success: false,
+      error: "An unexpected error occurred"
+    }
+  }
+};
