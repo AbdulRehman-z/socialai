9cda9888f5877c2480189f86faeb23fb4204b636
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-15 21:26:18 +0500

    Add constants for form field names and types

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const FIELD_NAMES = {
+  name: "Full Name",
+  email: "Email Address",
+  password: "Password",
+  confirmPassword: "Confirm Password",
+} as const
+
+export const FIELD_TYPES = {
+  name: "text",
+  email: "email",
+  password: "password",
+  confirmPassword: "password",
+} as const
