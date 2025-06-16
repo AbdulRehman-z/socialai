d0b24ea482ee4b8cd935cbc46223210641d6da51
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-16 12:43:06 +0500

    Refactor AuthLayout to integrate BrandPanel and adjust layout structure

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,15 +1,15 @@
-import AuthBackground from "@/components/custom/auth/auth-background"
+import BrandPanel from "@/components/custom/auth/brandPanel"
 import type { ReactNode } from "react"
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className="relative min-h-screen overflow-hidden">
-      <AuthBackground />
-      <div className="relative z-10 flex min-h-screen items-center justify-center p-4">
-        <div className="w-full max-w-md">{children}</div>
-      </div>
+  return <div className="min-h-screen flex">
+    <div className="flex-2">
+      {children}
     </div>
-  )
+    <div className="flex-1 min-h-screen w-full">
+      <BrandPanel />
+    </div>
+  </div>
 }
 
 export default AuthLayout
