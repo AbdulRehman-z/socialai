756d0fcb5d59d891440c5209f9f33c95e49a6527
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-19 20:20:41 +0500

    Update AuthLayout to conditionally render BrandPanel based on authentication routes

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,14 +1,21 @@
+"use client"
+
 import BrandPanel from "@/components/custom/auth/brandPanel"
 import type { ReactNode } from "react"
+import { usePathname } from 'next/navigation';
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
+  const router = usePathname();
+  // if the url path includes a /sign-in or /sign-up
+  const isAuth = router.includes('/sign-in') || router.includes('/sign-up');
+
   return <div className="min-h-screen flex">
     <div className="flex-2">
       {children}
     </div>
-    <div className="flex-1 min-h-screen w-full">
+    {isAuth && <div className="flex-1 min-h-screen w-full">
       <BrandPanel />
-    </div>
+    </div>}
   </div>
 }
 
