f4c3d5784cdd75033ec0d27a675eec0cb9bd5390
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-19 20:23:10 +0500

    Update email validation messages for clarity and add forgot password schema

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -1,7 +1,7 @@
 import { z } from "zod"
 export const signupSchema = z.object({
   name: z.string().min(1, { message: "Name must be at least 1 characters" }).max(100, { message: "Name must be at most 100 characters" }),
-  email: z.string().email({ message: "Invalid email address" }),
+  email: z.string().email({ message: "Provide valid email address" }),
   password: z.string().min(4, { message: "Password must be at least 4 characters" }).max(100, { message: "Password must be at most 100 characters" }),
   confirmPassword: z.string().min(4, { message: "Confirm password must be at least 4 characters" }).max(100, { message: "Confirm password must be at most 100 characters" }),
 }).refine((data) => data.password === data.confirmPassword, {
@@ -10,6 +10,10 @@ export const signupSchema = z.object({
 })
 
 export const signinSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
+  email: z.string().email({ message: "Provide valid email address" }),
   password: z.string().min(4, { message: "Password must be at least 4 characters" }).max(100, { message: "Password must be at most 100 characters" }),
+})
+
+export const forgotPasswordSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
 })
