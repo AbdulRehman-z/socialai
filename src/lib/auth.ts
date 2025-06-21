95003dac4ce76fcea44085ead75bfcab92f996cd
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-21 11:07:30 +0500

    Add email verification functionality to auth flow

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,6 +5,7 @@ import { emailOTP } from "better-auth/plugins";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { resend, sendEmailOTP, sendVerificationMail } from "./mail";
 import OTPEmail from "@/components/custom/emails/otpEmail";
+import EmailVerificationMail from "@/components/custom/emails/emailVerificationMail";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -21,8 +22,14 @@ export const auth = betterAuth({
     sendVerificationEmail: async ({ url, user, token }) => {
       const addressUrl = new URL(url)
       addressUrl.searchParams.set("callbackURL", "/verify")
+      await resend.emails.send({
+        from: 'Socialai <onboarding@resend.dev>',
+        to: user.email,
+        subject: "Verify your email",
+        react: EmailVerificationMail({ userEmail: user.email, verificationUrl: addressUrl.toString() }),
+      });
 
-      await sendVerificationMail(user.email, addressUrl)
+      // await sendVerificationMail(user.email, addressUrl)
     },
   },
   emailAndPassword: {
