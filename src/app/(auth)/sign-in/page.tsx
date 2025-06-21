907ae4c2ab90e4755001cc7a293fc18d0f73d455
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-21 18:06:58 +0500

    Implement session check in sign-in page and redirect authenticated users to home

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,23 @@
-"use client"
 
 import { signinAction } from "@/app/actions/auth/sign-in-action";
 import AuthForm from "@/components/custom/auth/authForm";
+import SigninView from "@/components/custom/views/auth/signinView";
+import { auth } from "@/lib/auth";
 import { signinSchema } from "@/lib/validations";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const SigninPage = () => {
+const SigninPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
+
+  if (!!session) {
+    redirect("/")
+  }
+
   return (
-    <AuthForm
-      type="SIGN_IN"
-      defaultValues={{
-        email: "",
-        password: "",
-      }}
-      schema={signinSchema}
-      onSubmit={signinAction}
-    />
+    <SigninView />
   )
 }
 
