de0d4653c05bf16f998df69700075c123b5d2702
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-24 17:24:42 +0500

    Replace header with DashboardHeader component in DashboardInset for improved structure

--- src/components/custom/dashboard/dashboardInset.tsx
+++ src/components/custom/dashboard/dashboardInset.tsx
@@ -3,22 +3,14 @@ import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar"
 import { ReactNode } from "react"
 import { ThemeToggle } from "../themeToggle"
 import { TooltipWrapper } from "../tooltipWrapper"
+import { Button } from "@/components/ui/button"
+import { SearchIcon } from "lucide-react"
+import { DashboardHeader } from "./dashboardHeader"
 
 const DashboardInset = ({ children }: { children: ReactNode }) => {
   return (
     <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-15 justify-between border-b">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-        </div>
-        <div className="pr-10">
-          <ThemeToggle />
-        </div>
-      </header>
+      <DashboardHeader />
       <main className="flex flex-1 flex-col gap-4 p-4 pt-0">
         {children}
       </main>
