34255c1fcbff035921a6482067011621fdd8a2e7
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-25 23:11:33 +0500

    Implement protected procedures for agent retrieval and creation with validation

--- src/modules/agents/server/proceduers.ts
+++ src/modules/agents/server/proceduers.ts
@@ -1,13 +1,26 @@
 import { agents, db } from "@/db";
-import { delay } from "@/lib/utils";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
+import { baseProcedure, createTRPCRouter, protectedBaseProcedure } from "@/trpc/init";
+import { agentsInsertSchema } from "../schema";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
+  getOne: protectedBaseProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+    const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id))
+    return existingAgent
+  }),
+  getMany: protectedBaseProcedure.query(async () => {
     const data = await db.select().from(agents)
     // throw new TRPCError({ code: "BAD_REQUEST" })
+    return data
+  }),
+  create: protectedBaseProcedure.input(agentsInsertSchema).mutation(async ({ ctx, input }) => {
+    const [data] = await db.insert(agents).values({
+      ...input,
+      userId: ctx.auth.user.id
+    }).returning()
 
     return data
   })
+
 })
