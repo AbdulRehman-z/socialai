19d12dfbad4a8cd35359db4d348b38f61be8cfdc
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-25 16:24:22 +0500

    Add delay utility function for asynchronous operations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,7 @@ export function cn(...inputs: ClassValue[]) {
 export function getInitials(name: string) {
   return name.split(' ').map(word => word[0]).join('').toUpperCase().slice(0, 2);
 }
+
+export const delay = async (intervalMS: number) => {
+  return await new Promise((resolve) => setTimeout(resolve, intervalMS))
+}
