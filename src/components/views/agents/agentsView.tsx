4505370e47e8c0f3f74cf527ca94810d42a74f6e
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-26 21:06:36 +0500

    Integrate filters into agent data fetching and add pagination component

--- src/components/views/agents/agentsView.tsx
+++ src/components/views/agents/agentsView.tsx
@@ -1,21 +1,30 @@
 "use client"
 
 import { columns } from "@/components/custom/agents/columns"
+import { DataPagination } from "@/components/custom/agents/dataPagination"
 import { DataTable } from "@/components/custom/agents/dataTable"
 import { EmptyAgentState } from "@/components/custom/emptyAgentState"
+import { useAgentsFilters } from "@/hooks/agents/use-agents-filters"
 import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query"
 
 export const AgentsView = () => {
+  const [filters, setFilters] = useAgentsFilters()
   const trpc = useTRPC()
   // the data won't be fetched on the client side(browser), it's already prefetched on the server.
   // so useSuspenseQuery is basically using the cached data already prefetched. If there is not prefetched data, it will fallback to useQuery().
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions())
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
+    ...filters,
+  }))
+
+  console.log({ filters })
 
   return (
     <div>
-      <DataTable columns={columns} data={data} />
-      {data.length === 0 && <EmptyAgentState title="Create your first agent" description="Create an agent to join meeting. Each agent will follow your instructions and can interact with participants during the call" />}
+      <DataTable columns={columns} data={data.items} />
+      <DataPagination page={filters.page} totalPages={data.totalPages
+      } onPageChange={(page) => setFilters({ page })} />
+      {data.items.length === 0 && <EmptyAgentState title="Create your first agent" description="Create an agent to join meeting. Each agent will follow your instructions and can interact with participants during the call" />}
     </div>
   )
 }
