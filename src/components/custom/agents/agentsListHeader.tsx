9648bef7d1d3b085e74baaa940f7771ca422addc
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-26 21:05:50 +0500

    Add filter clearing functionality to AgentsListsHeader component

--- src/components/custom/agents/agentsListHeader.tsx
+++ src/components/custom/agents/agentsListHeader.tsx
@@ -1,23 +1,46 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { PlusIcon } from "lucide-react"
+import { PlusIcon, XCircleIcon } from "lucide-react"
 import { NewAgentDialog } from "./newAgentDialog"
 import { useState } from "react"
+import { AgentsSearchFilters } from "./agentsSearchFilters"
+import { useAgentsFilters } from "@/hooks/agents/use-agents-filters"
+import { DEFAULT_PAGE } from "@/lib/constants"
 
 export const AgentsListsHeader = () => {
+  const [filters, setFilters] = useAgentsFilters()
   const [open, onOpenChange] = useState(false)
+
+  const areAnyFiltersModified = !!filters.search
+
+  const onClearFilters = () => {
+    setFilters({
+      ...filters,
+      search: "",
+      page: DEFAULT_PAGE
+    })
+  }
 
   return (
     <>
       <NewAgentDialog open={open} onOpenChange={onOpenChange} />
-      <div className="px-3 py-4 md:px-8 flex flex-col gap-y-4">
+      <div className="px-3 pt-5 md:px-8 flex flex-col gap-y-4">
         <div className="flex justify-between items-center">
           <h5 className="font-medium text-xl">My Agents</h5>
           <Button onClick={() => onOpenChange(!open)}>
             <PlusIcon />
             New Agent
           </Button>
+        </div>
+        <div className="flex items-center p-1 gap-x-2">
+          <AgentsSearchFilters />
+          {areAnyFiltersModified &&
+            <Button size="sm" variant={"outline"} onClick={onClearFilters} >
+              <XCircleIcon />
+              Clear
+            </Button>
+          }
         </div>
       </div>
     </>
