ebc52e22a37aafd0b1d63b03ad1fa0f452c0e797
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-26 21:07:09 +0500

    Enhance agent retrieval with pagination and search functionality

--- src/modules/agents/server/proceduers.ts
+++ src/modules/agents/server/proceduers.ts
@@ -1,19 +1,64 @@
 import { agents, db } from "@/db";
 import { baseProcedure, createTRPCRouter, protectedBaseProcedure } from "@/trpc/init";
-import { agentsInsertSchema } from "../schema";
+import { agentsGetManySchema, agentsInsertSchema } from "../schema";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
+
+
   getOne: protectedBaseProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-    const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id))
+    const [existingAgent] = await db.select(
+      {
+        meetings: sql<number>`5`,
+        ...getTableColumns(agents)
+      }
+    ).from(agents).where(eq(agents.id, input.id))
     return existingAgent
   }),
-  getMany: protectedBaseProcedure.query(async () => {
-    const data = await db.select().from(agents)
-    // throw new TRPCError({ code: "BAD_REQUEST" })
-    return data
+
+
+  getMany: protectedBaseProcedure.input(agentsGetManySchema).query(async ({ input, ctx }) => {
+    const { page, pageSize, search } = input
+
+    const data = await db.select({
+      meetings: sql<number>`5`,
+      ...getTableColumns(agents)
+    }).
+      from(agents)
+      .where(and(eq(agents.userId, ctx.auth.user.id),
+        search ? ilike(agents.name, `%${search}%`) : undefined))
+      .orderBy(desc(agents.createdAt), desc(agents.id))
+      .limit(pageSize)
+    // .offset((page - 1) * pageSize)
+    console.log({
+      res: (page - 1) * pageSize
+    })
+
+    const [total] = await db.select({
+      count: count()
+    }).
+      from(agents).
+      where(and(eq(agents.userId, ctx.auth.user.id),
+        search ? ilike(agents.name, `%${search}%`) : undefined))
+
+    const totalPages = Math.ceil(total.count / pageSize)
+
+
+    console.log({
+      // items: data,
+      total: total.count,
+      totalPages
+    })
+
+    return {
+      items: data,
+      total: total.count,
+      totalPages
+    }
   }),
+
+
   create: protectedBaseProcedure.input(agentsInsertSchema).mutation(async ({ ctx, input }) => {
     const [data] = await db.insert(agents).values({
       ...input,
