bef6296ff1b11eaf7d89d0d0773886e6874841dd
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-26 21:06:54 +0500

    Add pagination constants for default and boundary page sizes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -11,3 +11,9 @@ export const FIELD_TYPES = {
   password: "password",
   confirmPassword: "password",
 } as const
+
+
+export const DEFAULT_PAGE_SIZE = 10
+export const MIN_PAGE_SIZE = 1
+export const MAX_PAGE_SIZE = 100
+export const DEFAULT_PAGE = 1
