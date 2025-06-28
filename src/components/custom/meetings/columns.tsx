43e77941e6d539d85a602c49a0eeb3877b6be11f
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-28 16:28:21 +0500

    Add columns definition for meetings with status and duration formatting

---
+++ src/components/custom/meetings/columns.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import { Badge } from "@/components/ui/badge"
+import { cn } from "@/lib/utils"
+import { MeetingGetMany } from "@/modules/meetings/server/types"
+import { ColumnDef } from "@tanstack/react-table"
+import { format } from "date-fns"
+import humanizeDuration from "humanize-duration"
+import { CircleCheckIcon, CircleXIcon, ClockArrowUpIcon, ClockFadingIcon, CornerDownRightIcon, LoaderIcon } from "lucide-react"
+import { GeneratedAvatar } from "../generateAvatar"
+
+const formatDuration = (seconds: number) => {
+  return humanizeDuration(seconds * 1000, {
+    largest: 1,
+    round: true,
+    units: ["h", "m", "s"]
+  })
+}
+
+const statusIconMap = {
+  upcoming: ClockArrowUpIcon,
+  active: LoaderIcon,
+  completed: CircleCheckIcon,
+  processing: LoaderIcon,
+  cancelled: CircleXIcon
+}
+
+const statusColorMap = {
+  upcoming: "bg-yellow-400/90 text-yellow-800 border-yellow-800/5",
+  active: "bg-blue-400/90 text-blue-800 border-blue-800/5",
+  completed: "bg-green-400/90 text-green-800 border-green-800/5",
+  processing: "bg-gray-400/90 text-gray-800 border-gray-800/5",
+  cancelled: "bg-rose-400/90 text-rose-800 border-rose-800/5"
+}
+
+export const columns: ColumnDef<MeetingGetMany[number]>[] = [
+  {
+    accessorKey: "name",
+    header: "Meeting name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <span className="font-semibold capitalize">{row.original.name}</span>
+        <div className="flex items-center gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <CornerDownRightIcon className="size-3 text-muted-foreground" />
+            <span className="text-muted-foreground max-w-[200px] text-sm truncate capitalize">{row.original.agent.name}</span>
+          </div>
+          <GeneratedAvatar
+            seed={row.original.agent.name}
+            variant="botttsNeutral"
+            className="size-8"
+          />
+          <span className="text-sm text-muted-foreground">
+            {row.original.startedAt ? format(row.original.startedAt, 'MM d') : ""}
+          </span>
+        </div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => {
+      const Icon = statusIconMap[row.original.status as keyof typeof statusIconMap];
+
+      return (
+        <Badge
+          variant="outline"
+          className={cn(
+            "capitalize animate-pulse [&>svg]:size-3 text-muted-foreground",
+            statusColorMap[row.original.status as keyof typeof statusColorMap]
+          )}>
+          <Icon className={
+            cn(
+              row.original.status === "processing" && "animate-spin"
+            )
+          }
+          />
+          {row.original.status}
+        </Badge>
+      )
+    }
+  },
+  {
+    accessorKey: "duration",
+    header: "Duration",
+    cell: ({ row }) => (
+      <Badge
+        variant="outline"
+        className="capitalize [&>svg]:size-3 flex items-center gap-x-2">
+        <ClockFadingIcon className="text-primary" />
+        {row.original.duration ? formatDuration(row.original.duration) : "No duration"}
+      </Badge>
+    )
+  },
+]
