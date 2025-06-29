c2b12768445fe70a84e92af6c5eb67a5474059ff
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-29 23:17:25 +0500

    fix unconditional renders of headers

--- src/components/views/meetings/meetingView.tsx
+++ src/components/views/meetings/meetingView.tsx
@@ -1,21 +1,28 @@
 "use client"
 
+import { DataPagination } from "@/components/custom/dataPagination";
 import { DataTable } from "@/components/custom/dataTable";
 import { GenericEmptyState } from "@/components/custom/genericEmptyState";
 import { columns } from "@/components/custom/meetings/columns";
 import { MeetingsListsHeader } from "@/components/custom/meetings/meetingsListHeader";
+import { useMeetingsFilters } from "@/hooks/meetings/use-meetings-filters";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export const MeetingsView = () => {
+  const [filters, setFilters] = useMeetingsFilters()
+  const router = useRouter()
   const trpc = useTRPC()
-  const { data: meetings } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
+  const { data: meetings } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({
+    ...filters
+  }))
 
   return (
     <>
-      <MeetingsListsHeader />
-      <DataTable data={meetings.items} columns={columns} />
-      {meetings.items.length === 0 && <GenericEmptyState dialog="meeting" title="Create your first meeting"
+      <DataTable data={meetings.items} columns={columns} onRowClick={(row) => router.push(`/meetings/${row.id}`)} />
+      <DataPagination totalPages={meetings.totalPages} page={filters.page} onPageChange={(value) => setFilters({ page: value })} />
+      {meetings.items.length === 0 && <GenericEmptyState dialog="meeting" title="No meetings found"
         description="Schedule a meeting to connect with others.Each meeting lets you collaborate,interact and share your ideas with participants in realtime" />}
     </>
   );
