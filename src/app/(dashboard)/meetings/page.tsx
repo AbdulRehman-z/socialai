e803c66b6c61b110cbbf53b3e2f84ed4bafdc0f3
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-06-29 23:05:03 +0500

    Enhance MeetingsPage to load search parameters and include MeetingsListsHeader component

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,15 +1,24 @@
 import { GenericError } from "@/components/custom/genericError";
 import { GenericLoader } from "@/components/custom/genericLoader";
+import { MeetingsListsHeader } from "@/components/custom/meetings/meetingsListHeader";
 import { MeetingsView } from "@/components/views/meetings/meetingView";
 import { auth } from "@/lib/auth";
+import { loadSearchParams } from "@/modules/meetings/server/params";
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-const MeetingsPage = async () => {
+type MeetingsPageParams = {
+  params: Promise<SearchParams>
+}
+
+const MeetingsPage = async ({ params }: MeetingsPageParams) => {
+  const filters = await loadSearchParams(params)
+
   const session = await auth.api.getSession({
     headers: await headers()
   })
@@ -19,16 +28,21 @@ const MeetingsPage = async () => {
   }
 
   const queryClient = getQueryClient()
-  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}))
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({
+    ...filters
+  }))
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<GenericLoader title="Loading meetings..." description="Hold your horses! This might take few moments" />}>
-        <ErrorBoundary fallback={<GenericError title="Error Loading Meetings" description="Something goes off the rail ):.Try again" />}>
-          <MeetingsView />
-        </ErrorBoundary>
-      </Suspense>
-    </HydrationBoundary>
+    <>
+      <MeetingsListsHeader />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<GenericLoader title="Loading meetings..." description="Hold your horses! This might take few moments" />}>
+          <ErrorBoundary fallback={<GenericError title="Error Loading Meetings" description="Something goes off the rail ):.Try again" />}>
+            <MeetingsView />
+          </ErrorBoundary>
+        </Suspense>
+      </HydrationBoundary>
+    </>
   )
 }
 
