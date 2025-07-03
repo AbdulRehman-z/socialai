d644a043b0c0c8d56ccb171e73978ec18f357d13
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-07-03 20:27:26 +0500

    refactor GenericEmptyState component to support additional state types and corresponding SVGs

--- src/components/custom/genericEmptyState.tsx
+++ src/components/custom/genericEmptyState.tsx
@@ -3,30 +3,43 @@ import { Button } from "../ui/button";
 import { NewAgentDialog } from "./agents/newAgentDialog";
 import { EmptyStateSvg } from "./emptyStateSvg";
 import { NewMeetingDialog } from "./meetings/newMeetingsDialog";
+import { UpcomingStateSvg } from "./upcomingStateSvg";
+import { CancelledStateSvg } from "./cancelledStateSvg";
+import { ProcessingStateSvg } from "./processingStateSvg";
+import { CompletedStateSvg } from "./completedStateSvg";
 
 type GenericEmptyStateProps = {
   title: string;
   description: string;
-  dialog: "agent" | "meeting";
+  type?: "agent" | "meeting" | "upcoming" | "processing" | "cancelled" | "active"
+  | "completed"
 };
 
-export const GenericEmptyState = ({ title, description, dialog }: GenericEmptyStateProps) => {
+export const GenericEmptyState = ({ title, description, type }: GenericEmptyStateProps) => {
   const [open, onOpenChange] = useState(false);
 
   return (
     <>
-      {dialog === "agent" && <NewAgentDialog open={open} onOpenChange={onOpenChange} />}
-      {dialog === "meeting" && <NewMeetingDialog open={open} onOpenChange={onOpenChange} />}
+      {type === "agent" && <NewAgentDialog open={open} onOpenChange={onOpenChange} />}
+      {type === "meeting" && <NewMeetingDialog open={open} onOpenChange={onOpenChange} />}
       <div className="flex items-center justify-center py-10 px-8">
-        <div className="flex flex-col items-center gap-y-9">
-          <EmptyStateSvg width={240} height={240} />
-          <div className="flex flex-col items-center justify-center max-w-md mx-auto text-center gap-y-4">
+        <div className="flex flex-col items-center gap-y-1">
+          {type === "agent" || type === "meeting" && <EmptyStateSvg width={240} height={240} />}
+          {type === "upcoming" && <UpcomingStateSvg width={240} height={240} />}
+          {type === "processing" && <ProcessingStateSvg width={240} height={240} />}
+          {type === "cancelled" && <CancelledStateSvg width={240} height={240} />}
+          {type === "completed" && <CompletedStateSvg width={240} height={240} />}
+          {type === "active" && <UpcomingStateSvg width={240} height={240} />}
+          <div className="flex flex-col items-center justify-center max-w-md mx-auto text-center gap-y-2">
             <h1 className="text-xl font-medium">{title}</h1>
             <p className="text-sm text-muted-foreground">{description}</p>
           </div>
-          <Button size={"lg"} onClick={() => onOpenChange(true)}>
-            {dialog === "agent" ? "Create New Agent" : "Create New Meeting"}
-          </Button>
+          {type === "agent" && <Button size={"lg"} onClick={() => onOpenChange(true)}>
+            Create New Agent
+          </Button>}
+          {type === "meeting" && <Button size={"lg"} onClick={() => onOpenChange(true)}>
+            Create New Meeting
+          </Button>}
         </div>
       </div>
     </>
