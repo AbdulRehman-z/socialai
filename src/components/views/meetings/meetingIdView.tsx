cf99adaebb4957936028c892521ac29c46e316fc
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-07-03 20:29:21 +0500

    add MeetingIdView component to display various meeting statuses with corresponding components

--- src/components/views/meetings/meetingIdView.tsx
+++ src/components/views/meetings/meetingIdView.tsx
@@ -1,6 +1,11 @@
 "use client"
 
+import { ActiveStatus } from "@/components/custom/meetings/activeStatus";
+import { CancelledStatus } from "@/components/custom/meetings/cancelledStatus";
+import { CompletedStatus } from "@/components/custom/meetings/compeletedStatus";
 import { MeetingIdViewHeader } from "@/components/custom/meetings/meetingIdViewHeader";
+import { ProcessingStatus } from "@/components/custom/meetings/processingStatus";
+import { UpcomingStatus } from "@/components/custom/meetings/upcomingStatus";
 import { UpdateMeetingDialog } from "@/components/custom/meetings/updateMeetingDialog";
 import { useConfirm } from "@/hooks/use-confirm";
 import { useTRPC } from "@/trpc/client";
@@ -41,6 +46,13 @@ export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
     await removeMeeting.mutateAsync({ id: meetingId })
   }
 
+  const activeStatus = meeting.status === "active"
+  const completedStatus = meeting.status === "completed"
+  const cancelledStatus = meeting.status === "cancelled"
+  const upcomingStatus = meeting.status === "upcoming"
+  const processingStatus = meeting.status === "processing"
+
+
   return (
     <>
       <RemoveConfirmation />
@@ -49,16 +61,18 @@ export const MeetingIdView = ({ meetingId }: MeetingIdViewProps) => {
         open={updateMeetingDialogOpen}
         onOpenChange={setUpdateMeetingDialogOpen}
       />
-      <div className="flex flex-1 flex-col  md:px-8 px-3 py-4 gap-y-4">
+      <div className="flex flex-1 flex-col  md:px-8 sm:px-3 py-4 gap-y-4">
         <MeetingIdViewHeader
           meetingId={meeting.id}
           meetingName={meeting.name}
           onEdit={() => setUpdateMeetingDialogOpen(true)} // Open the update meeting dialog on click
           onRemove={handleRemoveMeeting}
         />
-        <pre>
-          {JSON.stringify(meeting, null, 2)}
-        </pre>
+        {completedStatus && <CompletedStatus meetingId={meeting.id} />}
+        {cancelledStatus && <CancelledStatus meetingId={meeting.id} />}
+        {processingStatus && <ProcessingStatus meetingId={meeting.id} />}
+        {activeStatus && <ActiveStatus meetingId={meeting.id} />}
+        {upcomingStatus && <UpcomingStatus meetingId={meeting.id} onCancelMeeting={() => { }} isCanceling={false} />}
       </div>
     </>
   );
