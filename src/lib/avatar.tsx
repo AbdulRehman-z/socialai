eeb94f02f860b825556fd9eb17ca64b0dfb4680b
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-07-03 20:29:35 +0500

    add: implement generateAvatarUri function to create avatars based on seed and variant

---
+++ src/lib/avatar.tsx
@@ -0,0 +1,24 @@
+import { botttsNeutral, initials } from "@dicebear/collection"
+import { createAvatar } from "@dicebear/core"
+
+type AvatarProps = {
+  seed: string,
+  variant: "botttsNeutral" | "initials"
+}
+
+export const generateAvatarUri = ({ seed, variant }: AvatarProps) => {
+  let avatar
+
+  if (variant === "botttsNeutral") {
+    avatar = createAvatar(botttsNeutral, {
+      seed
+    })
+  } else {
+    avatar = createAvatar(initials, {
+      seed: seed,
+      fontSize: 42,
+      fontWeight: 500
+    })
+  }
+  return avatar.toDataUri()
+}
