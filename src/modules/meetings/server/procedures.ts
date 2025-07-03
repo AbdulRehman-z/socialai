4ac3a44136c70dde517de26a88fcd4cbd81b9419
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-07-03 20:30:05 +0500

    feat: implement generateToken mutation and enhance meeting procedures with user authentication and avatar generation

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -5,30 +5,101 @@ import { TRPCError } from "@trpc/server";
 import { sql, getTableColumns, eq, and, ilike, desc, count } from "drizzle-orm";
 import z from "zod";
 import { meetingsGetManySchema, meetingsInsertSchema, meetingsUpdateSchema } from "./schema";
+import { generateAvatarUri } from "@/lib/avatar";
+import { streamVideo } from "@/lib/stream";
 
 export const meetingsRouter = createTRPCRouter({
   //////////////////////////////////////////////////
   /////////////////////////////////////////////////
+  // GENERATE TOKEN
+  generateToken: protectedBaseProcedure.mutation(async ({ ctx }) => {
+    const { user } = ctx.auth;
+
+    await streamVideo.upsertUsers([{
+      id: user.id,
+      name: user.name,
+      image: user.image ?? generateAvatarUri({ seed: user.name, variant: "initials" }),
+      role: "admin",
+    }])
+
+    const expirationTime = Math.floor(Date.now() / 1000) + 3600 // 1 hour
+    const issuedAt = Math.floor(Date.now() / 1000) - 60
+
+    const token = streamVideo.generateUserToken({
+      user_id: user.id,
+      exp: expirationTime,
+      validity_in_seconds: issuedAt
+    })
+
+    console.log({ token })
+    return token
+  }),
+
+  //////////////////////////////////////////////////
+  /////////////////////////////////////////////////
   // CREATE ONE
   create: protectedBaseProcedure.input(meetingsInsertSchema).mutation(async ({ ctx, input }) => {
-    const [data] = await db.insert(meetings).values({
+    const { user } = ctx.auth
+    const [createdMeeting] = await db.insert(meetings).values({
       ...input,
-      userId: ctx.auth.user.id
+      userId: user.id
     }).returning()
 
-    return data
+    const call = streamVideo.video.call("default", createdMeeting.id)
+    await call.create({
+      data: {
+        created_by_id: user.id,
+        custom: {
+          meetingId: createdMeeting.id,
+          meetingName: createdMeeting.name
+        },
+        settings_override: {
+          transcription: {
+            language: "en",
+            mode: "auto-on",
+            closed_caption_mode: "auto-on"
+          },
+          recording: {
+            mode: "auto-on",
+            quality: "1080p"
+          }
+        },
+      }
+    })
+
+    const [existingAgent] = await db.select().from(agents).
+      where(eq(agents.id, createdMeeting.agentId))
+
+    if (!existingAgent) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Agent not found"
+      })
+    }
+
+    await streamVideo.upsertUsers([{
+      id: existingAgent.id,
+      name: existingAgent.name,
+      role: "user",
+      image: generateAvatarUri({
+        seed: existingAgent.name,
+        variant: "botttsNeutral"
+      })
+    }])
+
+    return createdMeeting
   }),
 
   //////////////////////////////////////////////////
   /////////////////////////////////////////////////
   // UPDATE
   update: protectedBaseProcedure.input(meetingsUpdateSchema).mutation(async ({ input, ctx }) => {
+    const { user } = ctx.auth
 
-    console.log({ input })
     const [updateAgent] = await db.update(meetings)
       .set(input)
       .where(and(eq(meetings.id, input.id),
-        eq(meetings.userId, ctx.auth.user.id))).returning()
+        eq(meetings.userId, user.id))).returning()
 
     if (!updateAgent) {
       throw new TRPCError({
@@ -42,14 +113,21 @@ export const meetingsRouter = createTRPCRouter({
   //////////////////////////////////////////////////
   /////////////////////////////////////////////////
   // GET ONE
-  getOne: protectedBaseProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-    console.log({ input })
+  getOne: protectedBaseProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
+    const { user } = ctx.auth
 
     const [existingMeeting] = await db.select(
       {
-        ...getTableColumns(meetings)
+        ...getTableColumns(meetings),
+        agents: agents,
+        duration: sql<number>`EXTRACT(EPOCH FROM (ended_at-started_at))`.as("duration")
       }
-    ).from(meetings).where(eq(meetings.id, input.id))
+    ).from(meetings).
+      innerJoin(agents, eq(meetings.agentId, agents.id)).
+      where(
+        and(eq(meetings.id, input.id),
+          eq(meetings.userId, user.id))
+      )
 
     if (!existingMeeting) {
       throw new TRPCError({
@@ -65,6 +143,7 @@ export const meetingsRouter = createTRPCRouter({
   /////////////////////////////////////////////////
   // GET MANY
   getMany: protectedBaseProcedure.input(meetingsGetManySchema).query(async ({ input, ctx }) => {
+    const { user } = ctx.auth
     const { page, pageSize, search, agentId, status } = input
 
     const data = await db.select({
@@ -80,7 +159,7 @@ export const meetingsRouter = createTRPCRouter({
     }).
       from(meetings).
       innerJoin(agents, eq(meetings.agentId, agents.id)).
-      where(and(eq(meetings.userId, ctx.auth.user.id),
+      where(and(eq(meetings.userId, user.id),
         agentId ? eq(meetings.agentId, agentId) : undefined,
         status ? eq(meetings.status, status) : undefined,
         search ? ilike(meetings.name, `%${search}%`) : undefined))
@@ -113,9 +192,11 @@ export const meetingsRouter = createTRPCRouter({
   /////////////////////////////////////////////////
   // REMOVE
   remove: protectedBaseProcedure.input(z.object({ id: z.string() })).mutation(async ({ input, ctx }) => {
+    const { user } = ctx.auth;
+
     const [removeMeeting] = await db.delete(meetings)
       .where(and(eq(meetings.id, input.id),
-        eq(meetings.userId, ctx.auth.user.id))).returning()
+        eq(meetings.userId, user.id))).returning()
 
     if (!removeMeeting) {
       throw new TRPCError({
